6e81e48d0a1726cea90927f5b1640d2e55df42bc
Andy-K-Taua <andykalasinitaua@gmail>
2021-01-06 21:17:00 +1100

    no changes

--- src/components/ShowFlight.js
+++ src/components/ShowFlight.js
@@ -5,7 +5,23 @@ class ShowFlight extends React.Component {
   render() {
 
     return (
-      <div>Show Flight</div>
+      <div>Show Flight
+
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+          </div>
+        </div>
+      </div>
+
     );
 
   } // render()
