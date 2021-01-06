a47bf6bffa4a68adfcec0f9a77e6be6c0ca2db42
Sam Skinner <sam.sjskinner@gmail.com>
2021-01-06 12:54:43 +1100

    filled out most of CreateFlight form

--- src/components/CreateFlight.js
+++ src/components/CreateFlight.js
@@ -2,13 +2,59 @@ import React from 'react';
 
 class CreateFlight extends React.Component {
 
+  state = {
+    number: '',
+    origin: '',
+    destination: '',
+    date: '',
+    plane: '',
+    plane_id: ''
+  }
+
+  handleNumber = (ev) => {
+    this.setState({number: ev.target.value})
+  }
+  handleOrigin = (ev) => {
+    console.log('Origin: ', ev.target.value);
+  }
+  handleDestination = (ev) => {
+    console.log('Destination: ', ev.target.value);
+  }
+  handleDate = (ev) => {
+    console.log('Date: ', ev.target.value);
+  }
+  handlePlane = (ev) => {
+    console.log('Plane: ', ev.target.value);
+  }
+  handlePlaneID = (ev) => {
+    console.log('PlaneID: ', ev.target.value);
+  }
+
   render() {
 
     return (
       <div>
         <h2>Create New Flight:</h2>
         <form>
-
+          <label>Enter flight number:
+            <input type="text" placeholder="Flight number" onChange={this.handleNumber} />
+          </label><br />
+          <label>Enter Origin:
+            <input type="text" placeholder="Origin" onChange={this.handleOrigin} />
+          </label><br />
+          <label>Enter Destination:
+            <input type="text" placeholder="Destination" onChange={this.handleDestination} />
+          </label><br />
+          <label>Enter Date:
+            <input type="date" onChange={this.handleDate} />
+          </label><br />
+          <label>Enter Plane:
+            <input type="text" placeholder="Plane" onChange={this.handlePlane} />
+          </label><br />
+          <label>Enter Plane ID:
+            <input type="text" placeholder="Plane ID" onChange={this.handlePlaneID} />
+          </label><br />
+          <button>Create Flight!</button>
         </form>
       </div>
     );
