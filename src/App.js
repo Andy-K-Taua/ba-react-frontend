0f3fd3b39c650759ba4b5ace537d166c44a78334
brunabdesouza <bruna.bdesouza@gmail.com>
2021-01-07 10:42:53 +1100

    finally

--- src/App.js
+++ src/App.js
@@ -5,45 +5,16 @@ import SearchResults from './components/SearchResults';
 import ShowFlight from './components/ShowFlight';
 import CreatePlane from './components/CreatePlane';
 import CreateFlight from './components/CreateFlight';
-import axios from 'axios';
+
 import 'bootstrap';
 
 import {Route, /*Link, */HashRouter as Router} from 'react-router-dom';
 
-const RAILS_PLANES_BASE_URL = 'http://localhost:3000/flights';
+
 
 class App extends React.Component {
 
-  state = {
-    search: []
-  }
 
-  getSearchResults = () => {
-    axios.get( RAILS_PLANES_BASE_URL )
-    .then( ( res ) => {
-      console.log('response:', res.data );
-      this.setState({search: res.data })
-    })
-    .catch(console.warn);
-  }//getSearchResults
-
-
-
-  componentDidMount(){
-    console.log('Mounted');
-    this.getSearchResults();
-
-  }// componentDidMount
-
-  saveSearch = (search) => {
-    console.log('saveSearch()', {search});
-    axios.post(RAILS_PLANES_BASE_URL, {search: search })
-    .then( (res) => {
-      console.log('response from POST:', res.data);
-      this.setState({ search: [res.data, ...this.state.search ]})
-    })
-    .catch(console.warn);
-  }//saveSearch
 
   render() {
 
@@ -66,8 +37,6 @@ class App extends React.Component {
           <Route exact path="/addflight" component={CreateFlight} />
 
         </Router>
-
-          <SearchResults search={this.state.search} />
 
       </div>
     );
