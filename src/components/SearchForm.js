fccfae1aed5d26574d06ac01047a9b69d1d4892d
Sam Skinner <sam.sjskinner@gmail.com>
2021-01-06 11:54:57 +1100

    fixed SearchForm error and started on CreateFlight template

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,18 +2,16 @@ import React from 'react';
 import '../App.css';
 import axios from 'axios';
 
-import SearchForm from './SearchForm';
 
 class SearchForm extends React.Component {
 
   render() {
 
     return (
-      <div className>"Search Form"
+      <div className="searchForm">
       <h1>HEADING</h1>
       <hr />
 
-      <SearchForm onSearch={ this.performSearch} />
 
       </div>
     );
